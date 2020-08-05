475dfc84347a53fd654b695fad2aaa6769532f4e
Daniel Croce <dcroce@outlook.com>
2020-08-05 10:43:54 +0100

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "croced-notes-app-upload"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://9qppzik7lb.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_dW1Ce7yza",
+      APP_CLIENT_ID: "5uvda6qoegm5aee1g85bketbp2",
+      IDENTITY_POOL_ID: "eu-west-1:a71e3a77-d29f-49be-9874-d7f4e3f40264"
+    }
+  };
---
+++ src/containers/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+        <h1>404</h1>
+        <p>Page not found!</p>
+    </div>
+  );
+}
